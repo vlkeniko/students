9061212cb8f3c47831766a1e122d760c63155a4c
Eniko Valko <eaaenva@students.eaaa.dk>
2022-10-05 10:49:13 +0200

    adding some appifying codes

---
+++ serviceworker.js
@@ -0,0 +1,31 @@
+//Giving the cache a foldername
+const cacheName = "cache-students";
+
+//When the browser reads this for the first time, it caches all the files mentioned in the list.
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll([
+        "/students/",
+        "/students/index.html",
+        "/students/morten.png/",
+        "/students/nina.png",
+        "/students/olivia.png",
+      ]);
+    })
+  );
+});
+
+//If the user requests resource 
+// (file, html, css, image, css etc.)Then look 
+// for it onnline. If not available online, it 
+// gets the file from the cache
+
+self.addEventListener("fetch", function(event){
+    event.respondWith(
+        //The catch is for error handling.
+        fetch(event.request).catch(() =>
+        caches.open(cacheName).then(cache => cache.match(event.request))
+        )
+    )
+})
